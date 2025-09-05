72882b6960df15af20b160ad33de6bb7a4cc7c1a
Hamza623700 <6332234@edu.vaniercollege.qc.ca>
2025-09-05 16:51:44 -0400

    Update Lab2.py
    function- second commit

--- Lab2.py
+++ Lab2.py
@@ -3,10 +3,24 @@
 # Round up to one decimal point
 def calculate_height(h0, t):
     # TODO: Implement this function
-    pass  # Replace with your code
+    h0 = int(input("enter your height in m:"))
+    t = int(input("entere your time in seconds:"))
+    g = 9.81 
+    height = h0 + 1/2*g*t**2
+    print("the height of the ball", height,"m")
+
+    number = 54.905 
+    rounded_number = round(number,1)
+    print(rounded_number)
 
 # Function 2: Calculate the distance traveled by the car
 # This function should take the time t as input and return the distance traveled by the car.
 def calculate_car_distance(t):
     # TODO: Implement this function
-    pass  # Replace with your code
+    time = int(input("enter your time in seconds:"))
+    speed = int(input("enter your speed" "m/s" ))
+    distance = time*speed
+    print("the distance of the car", distance,"m")
+
+
+
